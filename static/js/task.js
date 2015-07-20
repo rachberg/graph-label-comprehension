535b2f8a2bb656b68fdd5ea0faa4781c67436f93
Rachel Bergmann <rachelbergmann@gmail.com>
2015-07-20 10:43:00 +0200

    changed Y/N to A/L so that it will be the same on different keyboards; rewrote welcome and instructions; next step is adding part 1 (numeracy test)

--- static/js/task.js
+++ static/js/task.js
@@ -8,26 +8,35 @@ var psiturk = new PsiTurk(uniqueId, adServerLoc, mode);
 
 var welcome_block = {
 	type: "text",
-	text: "Welcome to the experiment. Press any key to begin."
+	text: "<p>Welcome to the experiment.</p><p> There are two parts in this experiment. In Part 1, you will answer a brief" +
+"questionnaire and in Part 2 you will read a sentence and then decide whether" +
+"the following graph accurately depicts the information from the sentence.</p>" +
+"<h3>Please press any key to begin Part 1 of the experiment.</h3>"
 };
 
-var instructions_block = {
+var ex_instructions_block = {
 	type: "text",
-	text: "<p>In this experiment, you will read a sentence. Shortly after, a graph will appear in the center" +
-	" of the screen.</p><p>If the graph accurately represents <strong>all</strong> parts" + 
-	" of the information from the sentence shown, " + 
-	"type the letter Y on the keyboard. Otherwise, press the letter N on the keyboard. " + 
-	"It is important to press the Y or N key <strong>as fast as you can</strong>.</p>" +
-	"<p>Example:</p> <center><h3 class='center'>Mia buys fewer packs of gum each year.</h3></center>" +
-	"<div class='left center-content'><img width='400' src='/static/images/plots/good/Packs-vertical.png'></img>" + 
-	"<p class='small'><strong>Press the Y key</strong></p></div>" + 
-	"<div class='right center-content'><img width='400' src='/static/images/plots/wrong-data/Packs-topright.png'></img>" + 
-	"<p class='small'><strong>Press the N key</strong></p></div>" + "<p></p>" +
-	"<center><p>Press any key to begin.</p></center>",
-
+	text: "<p>Thank you for completing Part 1 of the experiment.</p>" +
+	"<p>In part 2, you will read a sentence. Shortly after, a graph will appear in the center " +
+	"of the screen.</p>" +
+	"<h3>Please place your left index finger on the 'A' letter on your keyboard and place your " +
+	"right index finger on the 'L' letter on your keyboard.</h3><p>If the graph accurately represents <strong>all</strong> " +
+	"parts f the information from the sentence shown, type the letter 'A.' If it does not, press the " +
+	"letter 'L.' It is important to press stye A or L key <strong>as fast as you can</strong> without making errors. Be " +
+	"sure that the figure matches the name, product, and data trend mentioned in the sentence.</p>" +
+	"<p>Please proceed to a example trial, where no data will be recorded.</p>" +
+	"<p>Press any key to begin the example trial.</p>",
 	timing_post_trial: 2000
 };
 
+// var ex_trial_block {
+	
+// };
+
+// var part2_instructions_block {
+// 	type: "text",
+// 	text: "Thank you for completing the example trial. Press any key to begin the experiment."
+// };
 
 var trial_types = jsPsych.randomization.repeat(["good", "good", "good", "good", "catch", "catch", "wrong-data", "wrong-data"], 1);
 
@@ -54,11 +63,11 @@ for (var i = 0; i < 8; i++) {
 	blocks.push(make_prompt_block(things_and_vignettes[i][1]));
 	var image = get_image(things_and_vignettes[i][0], rand_styles[i], trial_types[i]);
 	if(trial_types[i] == "good") {
-		blocks.push(make_image_block(image, '89'));
+		blocks.push(make_image_block(image, '65'));
 	}
 	else
 	{
-		blocks.push(make_image_block(image, '78'));
+		blocks.push(make_image_block(image, '76'));
 	}
 }
 
@@ -74,12 +83,12 @@ function make_image_block(stim, answer) {
 	return {
 		type: "categorize",
 		stimuli: stim,
-		choices: [89,78],
+		choices: [65,76],
 		is_html: true,
 		key_answer: [answer],
-		correct_text: "<p class='prompt'>Correct.</p>",
+		correct_text: "<p class='prompt' style='color:green; font-weight:bold;'><center>Your response is correct.</center></p>",
         // correct_text: "<p class='prompt'>Correct, this is a %ANS%.</p>",
-        incorrect_text: "<p class='prompt'>Incorrect.</p>"
+        incorrect_text: "<p class='prompt' style='color:red; font-weight:bold;'><center>Your response is incorrect.</center></p>"
         // incorrect_text: "<p class='prompt'>Incorrect, this is a %ANS%.</p>",
     };            
 }
@@ -95,7 +104,8 @@ var debrief_block = {
 var experiment_blocks = [];
 
 experiment_blocks.push(welcome_block);
-experiment_blocks.push(instructions_block);
+experiment_blocks.push(ex_instructions_block);
+// experiment_blocks.push(part2_instructions_block);
 experiment_blocks = experiment_blocks.concat(blocks);
 experiment_blocks.push(debrief_block);
 
